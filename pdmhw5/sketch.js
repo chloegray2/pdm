17021a269b982237ac879e99ed31d286c3a31454
chloegray2 <cgray39@lsu.edu>
2025-03-06 13:22:18 -0600

    hw5

--- pdmhw5/sketch.js
+++ pdmhw5/sketch.js
@@ -1,68 +1,125 @@
-touchEnded.Transport.bpm.value = 120;
-let button;
 let sampler;
+let isLoaded = false;
+let reverb;
+let soundOutput;
 
-function setup(){
-  sampler = new Tone.Players({
-    "sample1": "sample1.mp3",
-    "sample2": "sample2.mp3",
-    "sample3": "sample3.mp3",
-    "sample4": "sample4.mp3",
-    "sample5": "sample5.mp3",
-  }).toMaster();
+function preload() {
+  sounds = {
+    sample1: loadSound('assets/sample1.mp3'),
+    sample2: loadSound('assets/sample2.mp3'),
+    sample3: loadSound('assets/sample3.mp3'),
+    sample4: loadSound('assets/sample4.mp3'),
+    sample5: loadSound('assets/sample5.mp3')
+  };
+}
 
-  button1 = createButton('sample 1');
-  button1.position(10, 10);
-  button1.mousePressed(playSample1);
+function setup() {
+  createCanvas(400, 400);
+  userStartAudio();
+  
+  // Audio setup
+  soundOutput = getAudioContext();
+  reverb = new p5.Reverb();
+  reverb.disconnect();
+  reverb.connect();
+  reverb.drywet(0);
 
-  function playSample1(){
-    sampler.get('sample1').start()
-  }
+  // Canvas positioning
+  const canvas = document.querySelector('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.zIndex = '0';
 
-  button2 = createButton('sample 2');
-  button2.position(10, 30);
-  button2.mousePressed(playSample2);
+  // Controls container
+  const controls = createDiv();
+  controls.style.position = 'absolute';
+  controls.style.zIndex = '1';
+  controls.style.padding = '20px';
 
-  function playSample2(){
-    sampler.get('sample2').start()
-  }
+  let yPos = 10;
 
-  button3 = createButton('sample 3');
-  button3.position(10, 50);
-  button3.mousePressed(playSample3);
+  // Create buttons
+  button1 = createButton('sample 1 (loading...)');
+  button1.parent(controls);
+  button1.position(10, yPos);
+  button1.attribute('disabled', true);
+  yPos += 30;
 
-  function playSample3(){
-    sampler.get('sample3').start()
-  }
+  button2 = createButton('sample 2 (loading...)');
+  button2.parent(controls);
+  button2.position(10, yPos);
+  button2.attribute('disabled', true);
+  yPos += 30;
 
-  button4 = createButton('sample 4');
-  button4.position(10, 70);
-  button4.mousePressed(playSample4);
+  button3 = createButton('sample 3 (loading...)');
+  button3.parent(controls);
+  button3.position(10, yPos);
+  button3.attribute('disabled', true);
+  yPos += 30;
 
-  function playSample4(){
-    sampler.get('sample4').start()
-  }
+  button4 = createButton('sample 4 (loading...)');
+  button4.parent(controls);
+  button4.position(10, yPos);
+  button4.attribute('disabled', true);
+  yPos += 30;
 
-  button5 = createButton('sample 5');
-  button5.position(10, 90);
-  button5.mousePressed(playSample5);
+  button5 = createButton('sample 5 (loading...)');
+  button5.parent(controls);
+  button5.position(10, yPos);
+  button5.attribute('disabled', true);
+  yPos += 30;
 
-  function playSample5(){
-    sampler.get('sample5').start()
-  }
+  // Slider setup
+  slider = createSlider(0, 1, 0, 0.01);
+  slider.parent(controls);
+  slider.position(70, yPos);
+  slider.style('width', '100px');
+  slider.style('height', '15px');
+  slider.input(() => {
+    reverb.drywet(slider.value());
+  });
 
-  slider = createSlider(0, 1, 0);
-  slider.position(70, 140);
-  slider.style('width', '15px');
+  const reverbLabel = createDiv('reverb');
+  reverbLabel.parent(controls);
+  reverbLabel.position(10, yPos);
+  reverbLabel.style('color', 'black');
 
-  function effect(){
-    if (slider.value() == 1){
-      new Tone.Reverb(2).toMaster();
+  document.body.addEventListener('click', () => {
+    if (getAudioContext().state !== 'running') {
+      getAudioContext().resume();
     }
+  });
+
+  setTimeout(checkLoading, 100);
+}
+
+function checkLoading() {
+  if (Object.values(sounds).every(s => s.isLoaded())) {
+    enableButtons();
+  } else {
+    setTimeout(checkLoading, 100);
   }
 }
 
-function draw(){
-  createCanvas(400, 400);
-  text("reverb", 11, 150)
+function enableButtons() {
+  isLoaded = true;
+  const buttons = [button1, button2, button3, button4, button5];
+  buttons.forEach((btn, i) => {
+    btn.html(`sample ${i + 1}`);
+    btn.removeAttribute('disabled');
+    btn.mousePressed(() => playSample(i + 1));
+  });
+}
+
+function playSample(num) {
+  if (!isLoaded) return;
+  const sound = sounds[`sample${num}`];
+
+  sound.disconnect(); 
+  sound.connect(reverb); 
+  sound.stop(); 
+  sound.play(); 
+}
+
+function draw() {
+  background(220);
 }
